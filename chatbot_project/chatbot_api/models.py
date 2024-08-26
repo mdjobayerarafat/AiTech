fb5bf6f0049096875976b6d785a612634bde6c12
mdjobayerarafat <mdjobayerarafat@gmail.com>
2024-08-26 20:16:18 +0600

    models

--- chatbot_project/chatbot_api/models.py
+++ chatbot_project/chatbot_api/models.py
@@ -1,3 +1,7 @@
+
 from django.db import models
 
-# Create your models here.
+class ChatMessage(models.Model):
+    user_message = models.TextField()
+    bot_response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
